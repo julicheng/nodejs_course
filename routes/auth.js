d34f7766c6882932f2cc74fee716d969ff4d9c29
Juli Cheng <juli.cheng@axiell.com>
2021-03-05 15:36:40 +0000

    Check pw with bcrypt

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User } = require('../models/user');
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('invalid email or password');
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).send('invalid email or password');
+
+  res.send(true);
+});
+
+const validate = (req) => {
+  const schema = Joi.object({
+    email: Joi.string().required().min(5).max(255).email(),
+    password: Joi.string().required().min(5).max(255),
+  });
+  return schema.validate(req);
+};
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
@@ -13,6 +14,9 @@ router.post('/', async (req, res) => {
 
   // only pick name email and password from req.body to send
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  // change password to bcrypt one
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
