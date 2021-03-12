482cc462c41f47b3e46568b3ec5c4ad7c56c3b56
Juli Cheng <juli.cheng@axiell.com>
2021-03-12 14:28:27 +0000

    Add supertest

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express();
 
 require('./startup/logging')(); // function so we call it
 require('./startup/routes')(app); // function so we call it
-require('./startup/db')(); // function so we call it
 require('./startup/config')(); // function so we call it
+require('./startup/db')(); // function so we call it
 require('./startup/validation')(); // function so we call it
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => winston.info(`Listening on port ${port}....`));
+const server = app.listen(3000, () =>
+  winston.info(`Listening on port ${port}....`)
+);
+
+module.exports = server;
